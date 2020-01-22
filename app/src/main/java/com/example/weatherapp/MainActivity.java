9607c8fba3cdaedf9c95f97525b941e57d0b4158
Viktor Rosvall <virosen@yahoo.se>
2020-01-22 16:46:01 +0100

    Start on networking

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        XMLParser.parseURL("https://api.met.no/weatherapi/locationforecast/1.9/?lat=60.10;lon=9.58");
     }
+
+
 }
